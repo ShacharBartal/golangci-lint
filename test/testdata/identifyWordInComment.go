a12716bc825ec315da131d819b155c5cfabd2eff
ShacharBartal <shacharbartal@gmail.com>
2021-12-06 15:17:06 +0200

    full test passes

--- test/testdata/identifyWordInComment.go
+++ test/testdata/identifyWordInComment.go
@@ -1,3 +1,25 @@
 //args: -EidentifyWordInComment
 
 package testdata
+
+// nolint - before function declaration  // ERROR "word `nolint` is contained"
+func checkSpecialWordInCommentTst() {
+	//nolint - in function // ERROR "word `nolint` is contained"
+}
+
+func checkSpecialWordInCommentTst2() int {
+	return 1 // nolint:check for nolint with a linter report // ERROR "word `nolint` is contained"
+}
+
+func checkSpecialWordInCommentTst3() bool {
+	return true // nolint:check // with some explain // ERROR "word `nolint` is contained"
+}
+
+func checkSpecialWordInCommentTst4() string {
+	return "dominos" // dominos should not be in our code // ERROR "word `dominos` is contained"
+}
+
+// nolint free in the air // ERROR "word `nolint` is contained"
+
+func checkWordInCommentTst5() { // here please todo things // ERROR "word `todo` is contained"
+}
